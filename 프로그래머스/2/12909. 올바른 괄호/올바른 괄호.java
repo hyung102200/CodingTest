e2c626c03f62fcfc20194c6456b891942f78e4b4
hyung102200 <wohyoung1022@gmail.com>
2026-09-06 15:50:20 +0900

    [level 2] Title: 올바른 괄호, Time: 9.59 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,20 +3,17 @@ class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
         
-        for(int i=0; i<s.length(); i++) {
-            switch(s.charAt(i)) {
-                case '(': {
-                    stack.push('(');
-                    break;
-                }
-                case ')': {
-                    if(stack.isEmpty()) return false;
-                    stack.pop();
-                }
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                stack.push('(');
+            }
+            else {
+                if(stack.isEmpty()) return false;
+                stack.pop();
             }
         }
-        
         if(!stack.isEmpty()) return false;
+    
 
         return true;
     }
